038228bc05d03c3f1e60acf425afbb59d5742837
Adityo Rancaka <cakdityo@gmail.com>
2017-01-07 06:52:40 +0700

    Test search component

--- app/components/TodoSearch.jsx
+++ app/components/TodoSearch.jsx
@@ -10,18 +10,21 @@ export default class TodoSearch extends Component {
     }
 
     handleSearch(){
-        this.props.onSearch(this.showCompleted.checked, this.searchText.value);
+        let showCompleted = this.refs.showCompleted.checked;
+        let searchText = this.refs.searchText.value;
+
+        this.props.onSearch(showCompleted, searchText);
     }
 
     render(){
         return (
             <div>
                 <div>
-                    <input type="search" ref={ searchText => this.searchText = searchText } placeholder="Search todos" onChange={ this.handleSearch }/>
+                    <input type="search" ref="searchText" placeholder="Search todos" onChange={ this.handleSearch }/>
                 </div>
                 <div>
                     <label>
-                        <input type="checkbox" ref={ showCompleted => this.showCompleted = showCompleted } onChange={ this.handleSearch }/>
+                        <input type="checkbox" ref="showCompleted" onChange={ this.handleSearch }/>
                         Show Completed todos
                     </label>
                 </div>
